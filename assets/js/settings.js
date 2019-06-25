7e8e5e4a9059c752230a71e3b1643a8915bf58d8
Anne Livia <annelivia16@gmail.com>
2019-06-25 17:53:45 -0300

    update

--- assets/js/settings.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".btn-remover").addEventListener('click', e => {
-    e.preventDefault();
-    if(confirm("Tem certeza que deseja remover sua conta?")) {
-        window.location.href = "delete_bdadm.php";
-    }
-});
